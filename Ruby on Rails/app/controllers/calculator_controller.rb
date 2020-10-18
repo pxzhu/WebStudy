6252019b61c77df68922e377bd49ceff6f2f6481
pxzhu <wearan0nsgat@gmail.com>
2020-10-18 21:52:03 +0900

    2020-10-18

---
+++ Ruby on Rails/app/controllers/calculator_controller.rb
@@ -0,0 +1,8 @@
+class CalculatorController < ApplicationController
+  def add
+    
+  end
+  def result
+    @result = params[:first].to_i + params[:second].to_i
+  end
+end
---
+++ Ruby on Rails/config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # 예제
+  get '/add' => 'calculator#add'
+  post '/result' => 'calculator#result'
+  get '/result/:first/:second' => 'calculator#result'
+  # CR
+  get '/index' => 'home#index'
+  get '/write' => 'home#write'
+  post '/create' => 'home#create'
+  # UD
+  get '/modify/:post_id' => 'home#modify'
+  post '/update/:post_id' => 'home#update'
+  get '/delete/:post_id' => 'home#delete'
+end
